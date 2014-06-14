30db8ec2fe29f00a6e50c2cd373f2b281718e39e
Paul Tagliamonte <paultag@debian.org>
2014-06-13 21:04:53 -0400

    Imported Debian patch 1.0.0~dfsg1-1

---
+++ debian/repack.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+# Taken from the X Strike Force Build System
+
+set -e
+
+if ! [ -d debian/prune ]; then
+	exit 0
+fi
+
+if [ "x$1" != x--upstream-version ]; then
+	exit 1
+fi
+
+version="$2"
+filename="$3"
+
+if [ -z "$version" ] || ! [ -f "$filename" ]; then
+	exit 1
+fi
+
+dir="$(pwd)"
+tempdir="$(mktemp -d)"
+
+cd "$tempdir"
+tar xf "$dir/$filename"
+cat "$dir"/debian/prune/* | while read file; do rm -rvf */$file; done
+
+dfsgfilename="$(echo $filename | sed -E 's/(\.orig\.)/~dfsg1\1/')"
+#dfsgfilename="$(echo $filename | sed -E 's/(\.orig\.)/+dfsg1\1/')"
+tar -czf ${dir}/${dfsgfilename} *
+cd "$dir"
+rm -rf "$tempdir"
+echo "Done pruning upstream tarball into $dfsgfilename"
+
+exit 0
