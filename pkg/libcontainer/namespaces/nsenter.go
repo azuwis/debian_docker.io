b382ff2657c6a13b5df02d54443f05c1fddb645b
Zhong Jianxin <azuwis@gmail.com>
2014-06-30 11:17:05 +0800

    Use raw setns syscall for old versions of glibc

--- pkg/libcontainer/namespaces/nsenter.go
+++ pkg/libcontainer/namespaces/nsenter.go
@@ -13,6 +13,18 @@ package namespaces
 #include <sys/types.h>
 #include <unistd.h>
 
+// Use raw setns syscall for versions of glibc that don't include it (namely glibc-2.12)
+#if __GLIBC__ == 2 && __GLIBC_MINOR__ < 14
+#define _GNU_SOURCE
+#include <sched.h>
+#include "syscall.h"
+#ifdef SYS_setns
+int setns(int fd, int nstype) {
+  return syscall(SYS_setns, fd, nstype);
+}
+#endif
+#endif
+
 static const kBufSize = 256;
 
 void get_args(int *argc, char ***argv) {
